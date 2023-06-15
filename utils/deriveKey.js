4b89004637909cba17678d8798b3df971f78b558
sankalp <sankalp25103@gmail.com>
2023-06-15 21:44:54 +0530

    spinner created

--- utils/deriveKey.js
+++ utils/deriveKey.js
@@ -1,9 +1,9 @@
 import crypto from "crypto"
 export default async function deriveKey(givenString, salt) {
-    const iterations = 100000; // Number of iterations for key derivation
-    const keyLength = 32; // Key length in bytes
-    const digest = 'sha256'; // Digest algorithm for key derivation
+    const iterations = 100000; 
+    const keyLength = 32; 
+    const digest = 'sha256'; 
   
     return crypto.pbkdf2Sync(givenString, salt, iterations, keyLength, digest);
-  }
+}
 
