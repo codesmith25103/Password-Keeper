8ba19c87acba7fe6c3455db68d303100f8bc5417
lata <imlata1111@gmail.com>
2023-06-17 01:30:27 +0530

    add

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+import validator from 'validator';
  
 
 const userSchema = new mongoose.Schema({
@@ -21,6 +23,13 @@ const userSchema = new mongoose.Schema({
     confirmPassword: {
         type: String,
         required: [true, 'Please confirm your password'],
+        //This only works on create and save
+        validate: {
+            validator: function(el) {
+                return el === this.password;
+            },
+        message: 'Password is not same'
+        }
     },
     key: {
         type: String,
@@ -40,6 +49,19 @@ const userSchema = new mongoose.Schema({
     ]
 
 });
+
+userSchema.pre('save', async function(next){
+    //only run this function if password was actually modified
+    if(!this.isModified('password')) return next();
+    
+    //hash the password with the cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    //Delete passwordConfirm field
+    this.confirmPassword = undefined;
+    next();
+
+})
 
 const User= mongoose.model('User', userSchema);
 
