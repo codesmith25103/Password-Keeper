1c560f8053de6d460dc85d42f47df0aa9a2d5fa6
lata <imlata1111@gmail.com>
2023-06-17 13:44:29 +0530

    add

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,14 +1,30 @@
 const User = require('./../models/userModel');
+const jwt = require('jsonwebtoken');
+
 
 exports.signup = async( req, res, next) =>{
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name: req.body.username,
+        password: req.body.password
 
+    });
+const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPRESS_IN
+})
 
 res.status(201).json({
     status: 'success',
     data: {
-        user:newUser 
+        user:newUser  
     }
 
 })
+}
+
+ 
+exports.login = (req, res, next)=>{
+    const {username, password} = req.body;
+    if(!username || !password){
+        
+    }
 }
